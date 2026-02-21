f6dac42ca8a3e98c3a1f04f35e582eeadf76112b
Ludw <ludwig-geyer@web.de>
2026-02-21 17:15:52 +0100

    update

--- disko-config.nix
+++ disko-config.nix
@@ -6,47 +6,34 @@
         device = "/dev/sda";
         content = {
           type = "table";
-          format = "msdos";
-          partitions = [
-            {
-              name = "zfs";
-              start = "1M";
-              end = "100%";
+          format = "msdos"; # This is the MBR requirement
+          partitions = {
+            # In MBR, we define partitions as an attribute set
+            primary = {
+              size = "100%";
               part-type = "primary";
               bootable = true;
               content = {
                 type = "zfs";
                 pool = "zroot";
               };
-            }
-          ];
+            };
+          };
         };
       };
     };
     zpool = {
       zroot = {
         type = "zpool";
-
-        rootFsOptions = {
-          compression = "zstd";
-          acltype = "posixacl";
-          xattr = "sa";
-          "com.sun:auto-snapshot" = "false";
-        };
-        mountpoint = "/";
-        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
-
         datasets = {
-          "local" = {
-            type = "zfs_dataset";
-            mountpoint = "/local";
+          "root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options."com.sun:auto-snapshot" = "true";
           };
-          "local/nix" = {
-            type = "zfs_dataset";
-            mountpoint = "/nix";
-          };
+          # This is your requested data subvolume
           "data" = {
-            type = "zfs_dataset";
+            type = "zfs_fs";
             mountpoint = "/data";
           };
         };
