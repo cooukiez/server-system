920549612f47819800520e811157a9c488069d65
Ludw <ludwig-geyer@web.de>
2026-02-21 16:44:08 +0100

    update hardware config

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -29,46 +29,36 @@
     "sd_mod"
     "sdhci_pci"
   ];
-
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/20913f95-2766-4216-a920-8e080cd1fd17";
+    device = "/dev/disk/by-uuid/261fcaf4-c663-45e2-90f8-989fd0273cbc";
     fsType = "btrfs";
     options = [ "subvol=root" ];
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/B72C-E22D";
-    fsType = "vfat";
-    options = [
-      "fmask=0077"
-      "dmask=0077"
-    ];
-  };
-
   fileSystems."/data" = {
-    device = "/dev/disk/by-uuid/20913f95-2766-4216-a920-8e080cd1fd17";
+    device = "/dev/disk/by-uuid/261fcaf4-c663-45e2-90f8-989fd0273cbc";
     fsType = "btrfs";
     options = [ "subvol=data" ];
   };
 
   fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/20913f95-2766-4216-a920-8e080cd1fd17";
+    device = "/dev/disk/by-uuid/261fcaf4-c663-45e2-90f8-989fd0273cbc";
     fsType = "btrfs";
     options = [ "subvol=home" ];
   };
 
   fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/20913f95-2766-4216-a920-8e080cd1fd17";
+    device = "/dev/disk/by-uuid/261fcaf4-c663-45e2-90f8-989fd0273cbc";
     fsType = "btrfs";
     options = [ "subvol=nix" ];
   };
 
   fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/20913f95-2766-4216-a920-8e080cd1fd17";
+    device = "/dev/disk/by-uuid/261fcaf4-c663-45e2-90f8-989fd0273cbc";
     fsType = "btrfs";
     options = [ "subvol=var" ];
   };
