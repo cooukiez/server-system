fcda3087761521028bb0fdbd7c7aaaa34e887a18
Ludw <ludwig-geyer@web.de>
2026-02-22 22:13:28 +0100

    update keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  adminUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKs6Pw0mwJ5U/2HOkAID+ha9VFNp3Pm1eVF1HkFuYRsW admin@dhs";
+  adminUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzHMObNor0UyUOtyPjNQNHLNKf2RnAlQ1vJ50+tIt3v admin@dhs";
   dhsServer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICIQ0kQfmyndwCBlktgBvsAk3WBagbu0kKFGm+5ofXXv root@dhs";
 in
 {
