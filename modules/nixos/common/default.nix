724f08097c10b4f6387251e04d8cbd61a2b77a4f
Ludw <ludwig-geyer@web.de>
2026-02-22 16:27:49 +0100

    add nh

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -40,6 +40,13 @@
 
   programs.nix-ld.enable = true;
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/etc/nixos"; # sets NH_OS_FLAKE variable for you
+  };
+
   # zsh shell
   programs.zsh.enable = true;
 
