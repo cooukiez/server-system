e390094f9969996125802ef325f59f85568a356c
home-server <ludwig-geyer@web.de>
2026-03-11 15:09:29 +0100

    add immich

---
+++ modules/nixos/services/containers/103_immich.nix
@@ -0,0 +1,42 @@
+{
+  containers.immich = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "10.1.1.1";
+    localAddress = "10.1.1.102";
+
+    bindMounts."/dev/dri" = { 
+      hostPath = "/dev/dri"; 
+      isReadOnly = false; 
+    };
+
+    config = { config, pkgs, ... }: {
+      services.immich = {
+        enable = true;
+        port = 8000;
+        host = "0.0.0.0";
+        
+        # set to null for auto-detection of hardware acceleration
+        accelerationDevices = null; 
+      };
+
+      users.users.immich.extraGroups = [ "video" "render" ];
+
+      hardware.graphics = {
+        enable = true;
+        extraPackages = with pkgs; [
+          intel-vaapi-driver
+          vaapiVdpau
+          libvdpau-va-gl
+        ];
+      };
+
+      environment.variables = {
+        LIBVA_DRIVER_NAME = "i965";
+      };
+
+      networking.firewall.allowedTCPPorts = [ 8000 ];
+      system.stateVersion = "25.11";
+    };
+  };
+}
